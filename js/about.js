c9fa679ee3a0fced656d1f63dbdc5261280689e9
TheHandOfKing <a.marjanovic26@gmail.com>
2019-01-24 18:28:43 +0100

    Add files via upload

--- js/about.js
+++ js/about.js
@@ -1,3 +1,21 @@
+var localHolder = document.getElementById("placeHolderForCookies");
+var allStates =["United States", "America Latina", "Canada", "Mexico", "Puerto Rico","BELGIUM", "BULGARIA", "ČESKÁ", "SERBIA", "DANMARK", "DEUTSCHLAND", "ΕΛΛΑΔΑ", "ESPAÑA", "FINLAND", "FRANCE", "IRELAND", "ISRAEL", "ITALIA","Aegiptus", "Morroco", "Tunisia", "Rest of AfricaRest of Afr","Argentina", "Uruguay", "Chile", "Brasil", "America Latina","中国大陆", "台灣", "日本", "대한민국", "ประเทศไทย", "India","Australia", "New Zealand"];
+var localGetter = localStorage.getItem("state");
+if(localGetter === "Rest of Africa"){
+    document.title = "Hike Official Central Africa";
+    localHolder.innerHTML = "You are from  Central Africa";
+}
+else{
+    for(state in allStates){
+        if(localGetter == allStates[state]){
+            localHolder.innerHTML = "You are from  " + localGetter;
+            document.title = "Mike and Hike " + localGetter;
+            break;
+        }
+    }   
+}
+
+
 var classHolder = document.getElementById("biggerPart");
 for(i=0; i<3; i++){
     var newClasses = document.createElement("DIV");
@@ -19,18 +37,4 @@ for(i=0; i<secondClass.length; i++){
 var thirdClass = ["GET HELP","Shipping and Delivery","ReturnsPayment", "Options"];
 for(i=0; i<thirdClass.length; i++){
     classGet[2].innerHTML += "<p>" + thirdClass[i] + "</p>"; 
-}
-
-if(localGetter === "Rest of Africa"){
-    document.title = "Hike Official Central Africa";
-    localHolder.innerHTML = "You are from  Central Africa";
-}
-else{
-    for(state in allStates){
-        if(localGetter == allStates[state]){
-            localHolder.innerHTML = "You are from  " + localGetter;
-            document.title = "Mike and Hike " + localGetter;
-            break;
-        }
-    }   
 }
